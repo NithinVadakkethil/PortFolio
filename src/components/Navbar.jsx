a9dacd18f2f4339d7da71e41fda3082c7dd45e51
Nithin2255 <nithin@candorcode.com>
2023-06-05 12:40:32 +0530

    navbar section done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,8 +22,11 @@ const Navbar = () => {
           }}
         >
           <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
-          <p className='text-white text-[18px] font-bold cursor-pointer'>
-            Nithin <span className='sm:block hidden'>| Full Stack Developer</span></p>
+          <p className='text-white text-[18px] font-bold cursor-pointer flex'>
+            Nithin&nbsp;
+            <span className='sm:block hidden'>| Full Stack
+            Developer</span>
+          </p>
         </Link>
         <ul className='list-none hidden sm:flex flex-row gap-10'>
           {navLinks.map((link) => (
@@ -52,8 +55,11 @@ const Navbar = () => {
                 <li
                   key={link.id}
                   className={`${active === link.title ? "text-white" : "text-secondary"
-                    } hover:text-white text-[18px] font-medium cursor-pointer`}
-                  onClick={() => setActive(link.title)}
+                    } font-poppins font-medium cursor-pointer text-[16px]`}
+                  onClick={() => {
+                    setToggle(!toggle);
+                    setActive(link.title);
+                  }}
                 >
                   <a href={`#${link.id}`}>{link.title}</a>
                 </li>
